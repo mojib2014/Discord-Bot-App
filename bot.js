9a959f38641fd02a0be189452241bde47c6eec29
mojib2014 <mojib_monir@yahoo.com>
2021-05-25 19:57:27 -0700

    initial commit

---
+++ bot.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const Discord = require("discord.js");
+
+const client = new Discord.Client({
+  partials: ["MESSAGE"],
+});
+
+const BOT_PREFIX = "!";
+const MOD_ME_COMMAND = "mod-me";
+
+client.on("ready", () => {
+  console.log("Logged in as ", client.user.tag);
+});
+
+client.on("messageDelete", (msg) => {
+  msg.channel.send("Stop deleting messages");
+});
+
+client.on("message", (msg) => {
+  if (msg.content === "I love WDS") {
+    msg.react("❤️");
+  }
+
+  if (msg.content === `${BOT_PREFIX}${MOD_ME_COMMAND}`) {
+    modUser(msg.member);
+  }
+});
+
+function modUser(member) {
+  member.roles.add("846896691441696768");
+}
+
+client.login(process.env.BOT_TOKEN);
